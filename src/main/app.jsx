3709285b76e33a91d3ea5ea937f9e72cdd93def6
root <jonathan@hotmail.com>
2020-01-22 13:52:43 -0300

    start project

---
+++ src/main/app.jsx
@@ -0,0 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.min.css';
+
+import React from 'react'
+import Todo from '../main/app.jsx'
+
+export default props => (
+    <div className='container'>
+        <Todo />
+    </div>
+)
